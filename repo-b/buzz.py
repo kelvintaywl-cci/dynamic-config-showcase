0b6fca37f5e44f711201fbbc1b7898dda687598d
Kelvin Tay <kelvintaywl@gmail.com>
2022-08-27 08:57:44 +0900

    Update buzz.py

--- repo-b/buzz.py
+++ repo-b/buzz.py
@@ -1,1 +1,10 @@
-print("fizzbuzz")
+def fizzbuzz(n: int) -> str:
+    output = ""
+    if n % 3 == 0:
+        output += "fizz"
+    if n % 5 == 0:
+        output += "buzz"
+    return output or str(n)
+
+if __name__ == "__main__":
+    assert fizzbuzz(15) == "fizzbuzz"
